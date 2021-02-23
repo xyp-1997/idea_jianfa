73dd65b6644084d4bed346a50d9d2d9ab20629e1
xyp-1997 <316867814@qq.com>
2021-02-23 21:17:03 +0800

    剑法

---
+++ src/main/java/com/atguigu/Git/Jianfa.java
@@ -0,0 +1,9 @@
+package com.atguigu.Git;
+
+public class Jianfa {
+    public static void main(String[] args) {
+        System.out.println("剑法好啊！");
+        System.out.println("剑法好啊！");
+        System.out.println("剑法好啊！");
+    }
+}
